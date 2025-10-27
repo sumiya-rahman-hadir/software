07c274e27bb45a1412a8daaf8c9e200bb4a57876
sumiya-rahman-hadir <srahmanhadir@gmail.com>
2025-10-27 16:05:26 +0600

    First commit

---
+++ C#/ParamsRefOut/ParamsRefOut/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsRefOut
+{
+    internal class Program
+    {
+     public static int sum(int x, int y)
+        {
+            return x+y;
+        }
+        public static int sumParam(params int[] nunmList)
+        {
+            int total = 0;
+            foreach (int num in numList) ;
+
+        }
+        static void Main(string[] args)
+        {
+           int x= sum(4, 5);
+            Console.WriteLine("Sum is:" + x);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ hello world.c
@@ -0,0 +1,8 @@
+#include<stdio.in>
+
+int main(){
+printf("hello World\n");
+
+return 0;
+
+}
